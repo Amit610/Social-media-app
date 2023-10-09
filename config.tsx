2b80a409e9765a43d105d83888b411140b618bd2
Amit <amitjadhav610@gmail.com>
2023-10-09 13:43:25 +0530

    Initial commit

---
+++ config.tsx
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://amitjadhav610:QBTfXoEFCCR4yogb@cluster0.54czvnw.mongodb.net/?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
---
+++ util/validators.tsx
@@ -0,0 +1,58 @@
+
+interface ValidationErrors {
+  username?: string;
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+}
+
+export function validateRegisterInput(
+  username: string,
+  email: string,
+  password: string,
+  confirmPassword: string
+): { errors: ValidationErrors; valid: boolean } {
+  const errors: ValidationErrors = {};
+
+  if (username.trim() === "") {
+    errors.username = "Username must not be empty";
+  }
+  if (email.trim() === "") {
+    errors.email = "Email must not be empty";
+  } else {
+    const regEx =
+      /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+    if (!email.match(regEx)) {
+      errors.email = "Email must be a valid email address";
+    }
+  }
+  if (password === "") {
+    errors.password = "Password must not be empty";
+  } else if (password !== confirmPassword) {
+    errors.confirmPassword = "Passwords must match";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+}
+
+export function validateLoginInput(
+  username: string,
+  password: string
+): { errors: ValidationErrors; valid: boolean } {
+  const errors: ValidationErrors = {};
+
+  if (username.trim() === "") {
+    errors.username = "Username must not be empty";
+  }
+  if (password.trim() === "") {
+    errors.password = "Password must not be empty";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+}
